da4854c07f4007384611a248d745de0a3f82df13
tauseefshoaib <tauseefshoaib@gmail.com>
2021-07-10 02:18:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('Covid-19 Tracker')),
+            backgroundColor: Color(0xff0d47a1),
+          ),
+          body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: WebView(
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: "https://www.covid19india.org",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
